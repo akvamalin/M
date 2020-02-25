53e09d0fb48a178149952ee05dc30e1f744a9d30
Yevhenii Maliavka <yevhenii.maliavka@gmail.com>
2020-02-25 15:52:01 +0100

    Expose metrics  in sample-service

--- sample-service/src/app.py
+++ sample-service/src/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from prometheus_client import make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+from flask_prometheus_metrics import register_metrics
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+# provide app's version and deploy environment/config name to set a gauge metric
+register_metrics(app,  app_version="v0.0.2", app_config="dev")
+
+# Plug metrics WSGI app to your main app with dispatcher
+dispatcher = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
+
+run_simple(hostname='0.0.0.0', port=5001, application=dispatcher)
