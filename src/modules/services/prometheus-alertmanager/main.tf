3e0cd1838c282bea97b8b4af0557a91f97f79d2a
Yevhenii Maliavka <yevhenii.maliavka@gmail.com>
2020-02-26 14:44:31 +0100

    Remove wrong health check

--- src/modules/services/prometheus-alertmanager/main.tf
+++ src/modules/services/prometheus-alertmanager/main.tf
@@ -62,10 +62,6 @@ resource "aws_lb_target_group" "target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
-  health_check {
-    path = "/#/alerts"
-  }
 }
 
 resource "aws_lb_listener_rule" "sample_service" {
